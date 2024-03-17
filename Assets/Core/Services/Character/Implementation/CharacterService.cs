4ba6eaac4703747fb8c46e474d1e5f484601bdbc
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-17 16:14:47 +0300

    Implement ICharacterService

---
+++ Assets/Core/Services/Character/Implementation/CharacterService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Core.Factories;
+using Core.Services.Character.Interfaces;
+using UnityEngine;
+using Zenject;
+
+namespace Core.Services.Character.Implementation
+{
+    public class CharacterService : ICharacterService
+    {
+        public Dictionary<string, ICharacter> Characters { get; }
+
+        [Inject]
+        public CharacterService(CharacterFactory characterFactory)
+        {
+            Characters = new Dictionary<string, ICharacter>();
+
+            var character = new Character()
+                .SetView(characterFactory.Create("EnemyView"))
+                .SetPosition(Vector3.zero);
+
+            Characters.Add(character.Sid, character);
+        }
+    }
+}
