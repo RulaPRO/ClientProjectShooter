b7f11d891184ce8fc4a4490560c16815027c5098
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-17 16:15:33 +0300

    CharacterFactory

---
+++ Assets/Core/Factories/CharacterFactory.cs
@@ -0,0 +1,30 @@
+﻿using Characters;
+using UnityEngine;
+using Zenject;
+
+namespace Core.Factories
+{
+    public class CharacterFactory : IFactory<string, CharacterView>
+    {
+        private DiContainer diContainer;
+
+        [Inject]
+        public CharacterFactory(DiContainer diContainer)
+        {
+            this.diContainer = diContainer;
+        }
+
+        public CharacterView Create(string id)
+        {
+            var prefab = LoadCharacterPrefab(id);
+            var instance = diContainer.InstantiatePrefabForComponent<CharacterView>(prefab);
+
+            return instance;
+        }
+
+        private CharacterView LoadCharacterPrefab(string id)
+        {
+            return Resources.Load<CharacterView>($"Characters/{id}");
+        }
+    }
+}
