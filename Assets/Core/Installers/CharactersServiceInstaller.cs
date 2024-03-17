9bc86e576edcae8f10ffb650d8cb927f567eca4c
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-17 16:16:46 +0300

    CharactersServiceInstaller

---
+++ Assets/Core/Installers/CharactersServiceInstaller.cs
@@ -0,0 +1,25 @@
+﻿using Core.Factories;
+using Core.Services.Character.Implementation;
+using Core.Services.Character.Interfaces;
+using Zenject;
+
+namespace Core.Installers
+{
+    public class CharactersServiceInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container
+                .Bind<CharacterFactory>()
+                .FromNew()
+                .AsSingle();
+
+            Container
+                .Bind<ICharacterService>()
+                .To<CharacterService>()
+                .FromNew()
+                .AsSingle()
+                .NonLazy();
+        }
+    }
+}
