3825903218b8d172c720f8154fc15c895ed7b285
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-17 16:12:55 +0300

    Implement ICharacter

---
+++ Assets/Core/Services/Character/Implementation/Character.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Characters;
+using Core.Services.Character.Interfaces;
+using UnityEngine;
+
+namespace Core.Services.Character.Implementation
+{
+    public class Character : ICharacter
+    {
+        public string Sid { get; }
+        public CharacterView CharacterView { get; private set; }
+
+        public Character()
+        {
+            Sid = Guid.NewGuid().ToString();
+        }
+
+        public ICharacter SetView(CharacterView characterView)
+        {
+            CharacterView = characterView;
+
+            return this;
+        }
+
+        public ICharacter SetPosition(Vector3 value)
+        {
+            CharacterView.transform.position = value;
+
+            return this;
+        }
+    }
+}
