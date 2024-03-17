6cec5973c630413337403ee8f0a206e08c5a3366
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-17 16:12:01 +0300

    ICharacter

---
+++ Assets/Core/Services/Character/Interfaces/ICharacter.cs
@@ -0,0 +1,14 @@
+﻿using Characters;
+using UnityEngine;
+
+namespace Core.Services.Character.Interfaces
+{
+    public interface ICharacter
+    {
+        string Sid { get; }
+        CharacterView CharacterView { get; }
+
+        ICharacter SetView(CharacterView characterView);
+        ICharacter SetPosition(Vector3 value);
+    }
+}
