ef44aee5c84d0923d6bf2e38eee7ef003a6da93a
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-17 16:13:44 +0300

    ICharacterService

---
+++ Assets/Core/Services/Character/Interfaces/ICharacterService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Services.Character.Interfaces
+{
+    public interface ICharacterService
+    {
+        Dictionary<string, ICharacter> Characters { get; }
+    }
+}
