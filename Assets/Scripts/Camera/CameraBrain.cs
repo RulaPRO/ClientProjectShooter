22cd2aa3d7cc8ff62a5fe06992935a7706488eba
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-17 22:01:39 +0300

    Camera brain

---
+++ Assets/Scripts/Camera/CameraBrain.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraBrain : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float distance = 10f;
+    [SerializeField] private float height = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
+
+    private float currentRotation = 0f;
+
+    void Update()
+    {
+        currentRotation += Input.GetAxis("Mouse X") * rotationSpeed;
+        var rotation = Quaternion.Euler(0f, currentRotation, 0f);
+        var position = target.position - (rotation * Vector3.forward * distance);
+        position.y = target.position.y + height;
+        transform.position = position;
+        transform.LookAt(target.position);
+    }
+}
