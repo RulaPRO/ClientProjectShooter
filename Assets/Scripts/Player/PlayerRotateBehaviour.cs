31ec49d8c26b11320d6685c56bb6cb5c08f88287
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-18 01:55:54 +0300

    PlayerRotateBehaviour

---
+++ Assets/Scripts/Player/PlayerRotateBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using Core.Services.Input.Intrfaces;
+using UnityEngine;
+using Zenject;
+
+namespace Characters
+{
+    public class PlayerRotateBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float rotationSpeed = 5.0f;
+        
+        private IInputService inputService;
+
+        [Inject]
+        public void Construct(IInputService inputService)
+        {
+            this.inputService = inputService;
+        }
+
+        private void Update()
+        {
+            transform.rotation = Quaternion.Euler(0f, inputService.RotationY * rotationSpeed, 0.0f);
+        }
+    }
+}
