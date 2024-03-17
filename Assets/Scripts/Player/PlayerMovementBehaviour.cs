cae9f7fc7805d87636bd8cefcacaf224175ae30f
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-18 01:54:47 +0300

    PlayerMovementBehaviour

---
+++ Assets/Scripts/Player/PlayerMovementBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using Core.Services.Input.Intrfaces;
+using UnityEngine;
+using Zenject;
+
+namespace Characters
+{
+    public class PlayerMovementBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 10.0f;
+        
+        private IInputService inputService;
+
+        [Inject]
+        public void Construct(IInputService inputService)
+        {
+            this.inputService = inputService;
+        }
+
+        private void Update()
+        {
+            transform.Translate(inputService.Direction * moveSpeed * Time.deltaTime);
+        }
+    }
+}
