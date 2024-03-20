aa6764894cf35c1be6a0cb88c6ee66a3ced7086e
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-20 13:35:37 +0300

    WeaponServiceInstaller

---
+++ Assets/Core/Installers/WeaponServiceInstaller.cs
@@ -0,0 +1,19 @@
+﻿using Core.Services.Weapon.Implementation;
+using Core.Services.Weapon.Interfaces;
+using Zenject;
+
+namespace Core.Installers
+{
+    public class WeaponServiceInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container
+                .Bind<IWeaponService>()
+                .To<WeaponService>()
+                .FromNew()
+                .AsSingle()
+                .NonLazy();
+        }
+    }
+}
