4b9e43375dc3ab84320b695fbea7568cb9787cdb
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-20 11:52:52 +0300

    Weapon configs

---
+++ Assets/Balance/AssetsTypes/WeaponsConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Core.Services.Weapon.Implementation;
+using UnityEngine;
+
+namespace Balance.AssetsTypes
+{
+    [CreateAssetMenu(
+        menuName = "Config/" + nameof(WeaponsConfig),
+        fileName = nameof(WeaponsConfig))]
+    public class WeaponsConfig : ScriptableObject
+    {
+        [SerializeField] private List<WeaponConfigAsset> configs;
+
+        public WeaponConfigAsset GetConfig(WeaponType weaponType)
+        {
+            return configs.Find(_ => _.WeaponType == weaponType);
+        }
+    }
+}
