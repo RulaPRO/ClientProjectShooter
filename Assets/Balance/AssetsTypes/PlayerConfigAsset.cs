1987b8bc1ff67c3616d2499cda7eba837c117f30
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-20 14:49:01 +0300

    PlayerConfigAsset

---
+++ Assets/Balance/AssetsTypes/PlayerConfigAsset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Core.Services.Weapon.Implementation;
+using UnityEngine;
+
+namespace Balance.AssetsTypes
+{
+    [CreateAssetMenu(
+        menuName = "Balance/" + nameof(PlayerConfigAsset),
+        fileName = nameof(PlayerConfigAsset))]
+    public class PlayerConfigAsset : ScriptableObject
+    {
+        [SerializeField] private float defaultWalkSpeed;
+        [SerializeField] private List<WeaponType> startWeapons;
+
+        public List<WeaponType> StartWeapons => startWeapons;
+    }
+}
