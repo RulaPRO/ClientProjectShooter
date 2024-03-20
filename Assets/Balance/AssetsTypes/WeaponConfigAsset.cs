b9cc8a94bcf3fcc1f981db286449f871264c81ef
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-20 11:50:47 +0300

    WeaponConfigAsset

---
+++ Assets/Balance/AssetsTypes/WeaponConfigAsset.cs
@@ -0,0 +1,31 @@
+﻿using Core.Services.Weapon.Implementation;
+using UnityEngine;
+
+namespace Balance.AssetsTypes
+{
+    [CreateAssetMenu(
+        menuName = "Balance/" + nameof(WeaponConfigAsset),
+        fileName = nameof(WeaponConfigAsset))]
+    public class WeaponConfigAsset : ScriptableObject
+    {
+        [SerializeField] private WeaponType weaponType; // Тип оружия
+        [SerializeField] private int balletDamage; // Урон пули
+        [SerializeField] private float bulletRange; // Дальность полета пули
+        [SerializeField] private float bulletSpeed; // Скорость полета пули
+        [SerializeField] private float shootAccuracy; // Расброс пуль
+        [SerializeField] private int shootDelayMs; // Задержка между выстрелами
+        [SerializeField] private int magazineSize; // Обьем обоймы
+        [SerializeField] private int reloadTimeMs; // Время перезарядки
+
+        public WeaponType WeaponType => weaponType;
+        public int BalletDamage => balletDamage;
+        public float BulletRange => bulletRange;
+        public float BulletSpeed => bulletSpeed;
+        public float ShootAccuracy => shootAccuracy;
+        public int ShootDelayMs => shootDelayMs;
+        public int MagazineSize => magazineSize;
+        public int ReloadTimeMs => reloadTimeMs;
+
+
+    }
+}
---
+++ Assets/Core/Services/Weapon/Implementation/WeaponType.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Services.Weapon.Implementation
+{
+    public enum WeaponType
+    {
+        NONE = 0,
+        PISTOL = 1,
+        SHOTGUN = 2,
+        ASSAULT_RIFLE = 3,
+    }
+}
