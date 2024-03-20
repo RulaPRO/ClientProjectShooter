61459293554f9176492ad092e4d304bc418715b8
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-20 11:55:56 +0300

    Config installer

---
+++ Assets/Core/Installers/GameConfigInstaller.cs
@@ -0,0 +1,19 @@
+﻿using Balance.AssetsTypes;
+using UnityEngine;
+using Zenject;
+
+namespace Core.Installers
+{
+    [CreateAssetMenu(fileName = "ConfigInstaller", menuName = "Installers/ConfigInstaller")]
+    public class GameConfigInstaller : ScriptableObjectInstaller<GameConfigInstaller>
+    {
+        [SerializeField] private WeaponsConfig weapons;
+
+        public override void InstallBindings()
+        {
+            Container
+                .Bind<WeaponsConfig>()
+                .FromInstance(weapons);
+        }
+    }
+}
