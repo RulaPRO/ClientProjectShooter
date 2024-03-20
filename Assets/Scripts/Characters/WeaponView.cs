d355ef4c26301532b277d461516b1e7cd3d53071
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-20 14:55:11 +0300

    WeaponView

---
+++ Assets/Scripts/Characters/WeaponView.cs
@@ -0,0 +1,30 @@
+﻿using Balance.AssetsTypes;
+using UnityEngine;
+
+namespace Characters
+{
+    public class WeaponView : MonoBehaviour
+    {
+        [SerializeField] private Bullet prefabBullet;
+
+        private WeaponConfigAsset config;
+
+        public void Initialize(WeaponConfigAsset config)
+        {
+            this.config = config;
+        }
+
+        public void StartShoot()
+        {
+            var quaternion = Quaternion.Euler(
+                0.0f,
+                transform.rotation.y + Random.Range(-config.ShootAccuracy, config.ShootAccuracy),
+                0.0f);
+            
+            Instantiate(prefabBullet, transform.position, quaternion)
+                .SetDirection(transform.rotation)
+                .SetSpeed(config.BulletSpeed)
+                .SetDamage(config.BalletDamage);
+        }
+    }
+}
