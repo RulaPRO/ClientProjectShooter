6671b53bf2e1738f4e628f7c7049c9f2008e3732
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-21 15:35:07 +0300

    Character view

---
+++ Assets/Scripts/Characters/CharacterView.cs
@@ -0,0 +1,34 @@
+﻿using Core.Services.Character.Interfaces;
+using UI.Elements;
+using UnityEngine;
+using Zenject;
+
+namespace Characters
+{
+    public class CharacterView : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private CharacterUI characterUI;
+
+        private ICharacterService characterService;
+
+        private string characterId;
+
+        [Inject]
+        public void Construct(ICharacterService characterService)
+        {
+            this.characterService = characterService;
+        }
+
+        public void Initialize(string id)
+        {
+            characterId = id;
+            
+            characterUI.Initialize(id);
+        }
+
+        public void ApplyDamage(int value)
+        {
+            characterService.Characters[characterId].Health.DecreaseHealth(value);
+        }
+    }
+}
