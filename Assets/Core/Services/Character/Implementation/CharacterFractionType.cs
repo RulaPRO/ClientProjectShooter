7343ed079546e2de1b7f486312365c82d0c4bf77
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-24 16:37:14 +0300

    CharacterFractionType

---
+++ Assets/Core/Services/Character/Implementation/CharacterFractionType.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Services.Character.Implementation
+{
+    public enum CharacterFractionType
+    {
+        None = 0,
+        Player = 1,
+        Enemy = 2,
+        Neutral = 3,
+    }
+}
