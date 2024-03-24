5e5e417d30e38ecc647f580420d79edfb4aaaf5f
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-24 14:30:28 +0300

    App entry point

---
+++ Assets/Core/AppRunner/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using Core.Services.Character.Interfaces;
+using UnityEngine;
+using Zenject;
+
+namespace Core.AppRunner
+{
+    public class EntryPoint : IInitializable
+    {
+        private readonly ICharacterService characterService;
+
+        public EntryPoint(ICharacterService characterService)
+        {
+            this.characterService = characterService;
+        }
+
+        public void Initialize()
+        {
+            characterService.SpawnPlayer(Vector3.zero);
+        }
+    }
+}
